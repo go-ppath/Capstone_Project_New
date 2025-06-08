4b7ebbf163f2c9cbc8f70c32233656e1d621b998
go-ppath <goutomgswm@gmail.com>
2025-06-07 20:04:34 -0400

    starting summary.stats.tsv file to have tidy data

---
+++ summary_stats.sh
@@ -0,0 +1,7 @@
+# To summarize how many reads are in raw and trimmed data, how many aligned to the genome, and how many variants sites got from each sample
+
+# To have data in tidy format
+# added \t to give distinction in tsv file
+echo -e "filename\ttype\treads" > summary_stats.tsv
+
+
