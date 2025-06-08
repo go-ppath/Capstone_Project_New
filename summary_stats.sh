8a619b488f065fed5b5bba83d88cbf7c15a4c2ca
go-ppath <goutomgswm@gmail.com>
2025-06-07 20:39:04 -0400

    count aligned reads

--- summary_stats.sh
+++ summary_stats.sh
@@ -14,9 +14,23 @@
 
 # Get read length of trimmed file
 
-for trim in data/trimmed_fastq/*.fastq.gz
+#for trim in data/trimmed_fastq/*.fastq.gz
+#do
+ # echo $trim
+  #reads=$(zcat $trim | wc -l |awk '{print $1/4}')
+  #echo -e "$trim\ttrimmed\t$reads" >> summary_stats.tsv
+#done
+
+
+# To count how many reads aligned using samtools view
+# "samtools view" filters the reads only mapped to the reference genome
+# "-F 0x4" excludes reads flagged as unmapped, -F commands to exclude reads with certain FLAG, 0x4 gives the FLAG that "read is unmapped"
+
+module load SAMtools/1.18-GCC-12.3.0
+
+for bam in results/bam/*.sorted.bam
 do
-  echo $trim
-  reads=$(zcat $trim | wc -l |awk '{print $1/4}')
-  echo -e "$trim\ttrimmed\t$reads" >> summary_stats.tsv
+  echo $bam
+  reads=$(samtools view -F 0x4 "$bam" | wc -l)
+  echo -e "$bam\taligned\t$reads" >> summary_stats.tsv
 done
